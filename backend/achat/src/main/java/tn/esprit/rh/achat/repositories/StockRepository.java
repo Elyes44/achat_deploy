7ade3ba418192b8e02187c47f6f9943136d5707a
moez.felhi063719 <moez.felhi063719@gmail.com>
2023-10-12 16:39:02 +0100

    add JUnit tests to StockServices

--- backend/achat/src/main/java/tn/esprit/rh/achat/repositories/StockRepository.java
+++ backend/achat/src/main/java/tn/esprit/rh/achat/repositories/StockRepository.java
@@ -9,19 +9,7 @@ import java.util.List;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
-	
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	@Query("SELECT s FROM Stock s where s.qte< s.qteMin")
 	List<Stock> retrieveStatusStock();
 }
