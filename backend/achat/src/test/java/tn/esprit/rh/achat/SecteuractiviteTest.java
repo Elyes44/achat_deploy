51686a3c272a081973be3f8ae4a8bfea8862b9b7
Jendoubi Youssef <jendoubiy7@gmail.com>
2023-11-08 19:39:07 +0100

    commit

--- backend/achat/src/test/java/tn/esprit/rh/achat/SecteuractiviteTest.java
+++ backend/achat/src/test/java/tn/esprit/rh/achat/SecteuractiviteTest.java
@@ -1,84 +1,66 @@
 package tn.esprit.rh.achat;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.SecteurActivite;
 import tn.esprit.rh.achat.repositories.SecteurActiviteRepository;
 import tn.esprit.rh.achat.services.SecteurActiviteServiceImpl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
-public class SecteuractiviteTest {/*
-    @InjectMocks
-    private SecteurActiviteServiceImpl secteurActiviteService;
 
-    @Mock
+@SpringBootTest
+public class SecteuractiviteTest {
+
+    @Autowired
     private SecteurActiviteRepository secteurActiviteRepository;
 
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-    }
+    @Autowired
+    private SecteurActiviteServiceImpl secteurActiviteService;
+
+
+
 
     @Test
     public void testAddSecteurActivite() {
         SecteurActivite secteurActivite = new SecteurActivite();
-        when(secteurActiviteRepository.save(secteurActivite)).thenReturn(secteurActivite);
+        secteurActivite.setCodeSecteurActivite("S001");
+        secteurActivite.setLibelleSecteurActivite("Sec001");
 
         SecteurActivite addedSecteurActivite = secteurActiviteService.addSecteurActivite(secteurActivite);
 
-        verify(secteurActiviteRepository, times(1)).save(secteurActivite);
-        assertNotNull(addedSecteurActivite);
+        assertNotNull(addedSecteurActivite.getIdSecteurActivite());
+        assertEquals("S001", addedSecteurActivite.getCodeSecteurActivite());
+        assertEquals("Sec001", addedSecteurActivite.getLibelleSecteurActivite());
     }
-
-     @Test
-    public void testDeleteSecteurActivite() {
-        Long id = 1L;
-        doNothing().when(secteurActiviteRepository).deleteById(id);
-
-        secteurActiviteService.deleteSecteurActivite(id);
-
-        verify(secteurActiviteRepository, times(1)).deleteById(id);
-    }
-
     @Test
     public void testUpdateSecteurActivite() {
         SecteurActivite secteurActivite = new SecteurActivite();
-        when(secteurActiviteRepository.save(secteurActivite)).thenReturn(secteurActivite);
+        secteurActivite.setIdSecteurActivite(1L); // Assuming an SecteurActivite with ID 1 exists in the database
+        secteurActivite.setLibelleSecteurActivite("updated");
 
         SecteurActivite updatedSecteurActivite = secteurActiviteService.updateSecteurActivite(secteurActivite);
 
-        verify(secteurActiviteRepository, times(1)).save(secteurActivite);
-        assertNotNull(updatedSecteurActivite);
+        assertEquals("updated", updatedSecteurActivite.getLibelleSecteurActivite());
     }
-
     @Test
     public void testRetrieveSecteurActivite() {
+        // Assuming an SecteurActivite with ID 1 exists in the database
         Long id = 1L;
-        SecteurActivite secteurActivite = new SecteurActivite();
-        when(secteurActiviteRepository.findById(id)).thenReturn(Optional.of(secteurActivite));
 
-        SecteurActivite retrievedSecteurActivite = secteurActiviteService.retrieveSecteurActivite(id);
-
-        verify(secteurActiviteRepository, times(1)).findById(id);
-        assertNotNull(retrievedSecteurActivite);
+        SecteurActivite secteurActivite = secteurActiviteService.retrieveSecteurActivite(id);
     }
+  /*  @Test
+    public void testDeleteSecteurActivite() {
+        // Assuming an SecteurActivite with ID 1 exists in the database
+        Long id = 5L;
 
-    @Test
-    public void testRetrieveAllSecteurActivite() {
-        List<SecteurActivite> secteurActiviteList = new ArrayList<>();
-        when(secteurActiviteRepository.findAll()).thenReturn(secteurActiviteList);
+        secteurActiviteService.deleteSecteurActivite(id);
 
-        List<SecteurActivite> retrievedList = secteurActiviteService.retrieveAllSecteurActivite();
-
-        verify(secteurActiviteRepository, times(1)).findAll();
-        assertNotNull(retrievedList);
+        // Verify that the SecteurActivite is deleted (you can use Mockito to verify the delete method was called)
     }*/
 }
+
+// Write similar test methods for other service operations
+
